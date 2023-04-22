4833bab66c539ef18e26a8f7b3714c17e8fcf70f
pva18 <petervarga0018@gmail.com>
2023-04-22 10:16:32 +0200

    Módosítási idő szerinti optimalizálás Python script-ben.

--- ESP01_WIFI_MODULE_CODE/convert_text_to_cstring.py
+++ ESP01_WIFI_MODULE_CODE/convert_text_to_cstring.py
@@ -3,6 +3,15 @@ import os
 
 
 def text_to_cstring(input_file, output_file=None):
+    # Create the output file name if it was not provided
+    if output_file is None:
+        output_file = os.path.splitext(input_file) + '_cstring.txt'
+
+    # Check if the output file already exists and wether it the the input file was modified since the output file was created
+    if os.path.exists(output_file) and os.path.getmtime(input_file) < os.path.getmtime(output_file):
+        print(f"Output file '{output_file}' already exists and is up to date. Skipping...\r\n")
+        return
+
     # Read the text file
     with open(input_file, "r") as f:
         text = f.read()
@@ -13,8 +22,6 @@ def text_to_cstring(input_file, output_file=None):
     text = text.replace('\n', '\\n\"\n\"')
 
     # Write the text to a file in C string format
-    if output_file is None:
-        output_file = os.path.splitext(input_file) + '_cstring.txt'
     with open(output_file, "w") as f:
         # f.write(f'static const char {os.path.splitext(os.path.basename(output_file))[0]}[] PROGMEM = "{text}";')
         f.write(f'"{text}"')
