845e08d2a9c7d891587dff9d1cdc3bc1aa4f88d1
Péter Varga <petervarga0018@gmail.com>
2023-08-15 20:59:48 +0200

    Add directory check and Windows batch script

---
+++ ESP01_WIFI_MODULE_CODE/prebuild.bat
@@ -0,0 +1,11 @@
+set CONVERTER_SCRIPT=convert_to_cstring.py
+set TEXT_OUTPUT_PATH=RobotKar_WIFI\cstring_text
+
+if not exist %TEXT_OUTPUT_PATH%\ mkdir %TEXT_OUTPUT_PATH%
+
+echo This directory contains automatically generated files. Do not modify them. Edit the original text files instead, then run the prebuild script.> %TEXT_OUTPUT_PATH%\README.txt
+
+python %CONVERTER_SCRIPT% GUI_HTML\robotkar_gui_main.html %TEXT_OUTPUT_PATH%\robotkar_gui_main_cstring.h
+python %CONVERTER_SCRIPT% GUI_HTML\robotkar_back_to_index.html %TEXT_OUTPUT_PATH%\robotkar_back_to_index_cstring.h
+python %CONVERTER_SCRIPT% RobotKar_WIFI\default_json\default_configuration.json %TEXT_OUTPUT_PATH%\default_configuration_cstring.h
+python %CONVERTER_SCRIPT% RobotKar_WIFI\default_json\default_data.json %TEXT_OUTPUT_PATH%\default_data_cstring.h
