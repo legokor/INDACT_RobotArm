767cc2179ae4c424a1a21d0ca8816688ce76cd61
Péter Varga <petervarga0018@gmail.com>
2023-08-29 22:54:06 +0200

    Add missing documentation

--- ESP01_WIFI_MODULE_CODE/RobotArmWifi/ServerManager.h
+++ ESP01_WIFI_MODULE_CODE/RobotArmWifi/ServerManager.h
@@ -5,9 +5,9 @@
  * @file ServerManager.h
  * @author Péter Varga (petervarga0018@gmail.com)
  * @date 2023-08-29
- * 
+ *
  * @brief This file contains the declaration of the ServerManager class.
- * 
+ *
  */
 
 #include <ESP8266WiFi.h>
@@ -18,15 +18,43 @@
 namespace robot_arm_wifi
 {
 
+    /**
+     * @brief Class for handling the server.
+     */
     class ServerManager
     {
     public:
+        /**
+         * @brief Constructor with action callback initialization.
+         *
+         * @param action_callback The callback function to be called when an action is received.
+         */
         ServerManager(std::function<void(const std::string &)> action_callback);
 
+        /**
+         * @brief Set the layout configuration.
+         *
+         * @param layout_configuration The layout configuration to be set.
+         */
         void SetLayoutConfiguration(const std::string &layout_configuration);
+
+        /**
+         * @brief Set the data update.
+         *
+         * @param data_update The data update to be set.
+         */
         void SetDataUpdate(const std::string &data_update);
+
+        /**
+         * @brief Set the action callback function.
+         *
+         * @param action_callback The callback function to be called when an action is received.
+         */
         void SetActionCallback(std::function<void(const std::string &)> action_callback);
 
+        /**
+         * @brief Run the server.
+         */
         void Run();
 
     private:
