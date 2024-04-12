47405ce46a5f2adf49496527d22da2e9cea022f2
Péter Varga <petervarga0018@gmail.com>
2024-04-12 10:59:23 +0200

    Update to latest IDE version

--- RobotArmController/Core/Src/main.c
+++ RobotArmController/Core/Src/main.c
@@ -78,6 +78,7 @@ void MX_FREERTOS_Init(void);
   */
 int main(void)
 {
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -136,13 +137,14 @@ int main(void)
   /* Init scheduler */
   osKernelInitialize();
 
-  /* Call init function for freertos objects (in freertos.c) */
+  /* Call init function for freertos objects (in cmsis_os2.c) */
   MX_FREERTOS_Init();
 
   /* Start scheduler */
   osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
